fb83c9d4a493d9901fc1c7ff6b76ae59256894e7
Terehina Maria <tma8085@mail.ru>
2024-05-21 17:17:32 +0400

    decorat blocks in main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("lol");
+const swiperGalery = new Swiper('.swiper-galery', {
+    slidesPerView: "auto",
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.galery-button-next',
+      prevEl: '.galery-button-prev',
+    },
+  });
