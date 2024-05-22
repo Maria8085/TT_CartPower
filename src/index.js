74c263330c6ff6a51f214389cde2cab973eabc45
Maria8085 <52406953+Maria8085@users.noreply.github.com>
2024-05-23 00:05:48 +0400

    add decor all elements

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ const swiperGalery = new Swiper('.swiper-galery', {
       prevEl: '.galery-button-prev',
     },
   });
+
+const swiperCollection = new Swiper('.collection-slider .product-slider', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop:true,
+    navigation: {
+        nextEl: '.collection-button-next',
+        prevEl: '.collection-button-prev',
+    },
+});
+
+const swiperRecommendations = new Swiper('.recommendations-slider .product-slider', {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    loop:true,
+    navigation: {
+        nextEl: '.recommendations-button-next',
+        prevEl: '.recommendations-button-prev',
+    },
+});
